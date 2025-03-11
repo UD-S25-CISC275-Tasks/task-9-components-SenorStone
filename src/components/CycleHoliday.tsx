3e00e68f82c0564ad963627a2b9749ded43fe18b
Matthew Stone <mbstone@udel.edu>
2025-03-10 20:46:45 -0400

    More stuff done!

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+export type Holiday =
+    | "World Parrot Day"
+    | "Cheese Day"
+    | "Best Friend Day"
+    | "Go Fishing Day"
+    | "Dog Day";
+
 export function CycleHoliday(): React.JSX.Element {
     return <div>Cycle Holiday</div>;
 }
