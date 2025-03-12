ac0657f0d07de7878e258abf1ff40795bcbd5d77
Matthew Stone <mbstone@udel.edu>
2025-03-12 15:02:46 -0400

    Please be done

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,17 @@
 // You will not need to modify this file
 
 import React from "react";
-<<<<<<< HEAD
 import ReactDOM from "react-dom";
-=======
-import ReactDOM from "react-dom/client";
->>>>>>> origin
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-<<<<<<< HEAD
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
     document.getElementById("root")
-=======
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
->>>>>>> origin
 );
 
 // If you want to start measuring performance in your app, pass a function
